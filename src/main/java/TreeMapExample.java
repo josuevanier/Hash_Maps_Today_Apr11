d1c46a312b6e0e28bfe52a3ccba512bd5402e7f9
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-04-11 15:15:00 -0400

    A tree keep the order in ascending order based on the key value

---
+++ src/main/java/TreeMapExample.java
@@ -0,0 +1,13 @@
+import java.util.TreeMap;
+
+public class TreeMapExample {
+    public static void main(String[] args) {
+
+
+        TreeMap<Integer, String> map = new TreeMap<>();
+        map.put(1,"sas2d");
+        map.put(2,"saassd");
+        map.put(3,"ssasd");
+        System.out.println("s");
+    }
+}
