8e41340d10c166c50834de4005f1203353249900
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-04-11 15:34:57 -0400

    I dont get nothing but need to study the concept : TreeMap

--- src/main/java/TreeMapExample.java
+++ src/main/java/TreeMapExample.java
@@ -1,18 +1,45 @@
+import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class TreeMapExample {
+   static class Student{
+        int rollNo;
+        String name, address;
+
+        public Student(int rollNo, String name, String address) {
+            this.rollNo = rollNo;
+            this.name = name;
+            this.address = address;
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "rollNo=" + rollNo +
+                    ", name='" + name + '\'' +
+                    ", address='" + address + '\'' +
+                    '}';
+        }
+
+
+    }
+    static class SortByRoll implements Comparator<Student>{
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o1.rollNo - o2.rollNo;
+        }
+    }
     public static void main(String[] args) {
 
 
-        TreeMap<Integer, String> map = new TreeMap<>();
-        map.put(1,"sas2d");
-        map.put(2,"saassd");
-        map.put(3,"ssasd");
-        System.out.println("s");
+        TreeMap<Student, Integer> map = new TreeMap<Student, Integer>(new SortByRoll());
+        //Mapping String values int keys
+        map.put(new Student(1,"s","2"),2);
+        map.put(new Student(6,"a","2"),4);
+        map.put(new Student(2,"b","1"),3);
 
-        for(Map.Entry m : map.entrySet()){
-            System.out.println(m.getKey() + " " + m.getValue());
-        }
+        System.out.println("Tree Map:" + map);
+
     }
 }
